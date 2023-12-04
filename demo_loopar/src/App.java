501823ecac1f119ab5b56c15b242f5f5d18e0e74
Hugo Frimodig <Hugo.Frimodig@elev.ga.ntig.se>
2023-12-04 08:46:37 +0100

    first commit

---
+++ demo_loopar/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+   
+        tal =0;
+
+        int[] array = new int[5];
+        array[0]=4;
+        array[1]=8;
+        array[2]=12;
+        array[3]=16;
+        array[4]=20;
+        array[5]=24;
+    
+
+        while(tal > -10)
+        {
+            tal=tal-2; // tal++
+            System.out.println("Summan av talen blir "+tal);
+        }
+
+    }
+}
---
+++ demo_metoder/src/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      Scanner scan = new Scanner (System.in); 
+      System.out.println("Ange en bas");
+      int tal1 = scan.nextInt();
+      System.out.println("Ange exponent: ");
+      Int exponent = Scanner.nextInt();
+
+      System.out.println("Resultatet är "+Resultatet);
+
+      
+
+
+
+
+    }
+
+    static int exponent(int bas, int exponent)
+    {
+        
+
+        
+
+        
+    }
+}
